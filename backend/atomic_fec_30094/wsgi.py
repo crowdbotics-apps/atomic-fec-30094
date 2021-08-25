8ef72ccaf5dc77e359d89c6acaef985550472869
Crowdbotics <team@crowdbotics.com>
2021-08-25 05:50:22 +0000

    Update models

--- backend/atomic_fec_30094/wsgi.py
+++ backend/atomic_fec_30094/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atomic_fec_30094.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "atomic_fec_30094.settings")
 
 application = get_wsgi_application()
