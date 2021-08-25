ee4e12cce7a8ac96cc83ab1830751dfb794943c6
Crowdbotics <team@crowdbotics.com>
2021-08-25 05:48:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "atomic_fec_30094Identifier",
+  url: "https://atomic_fec_30094Identifier.botics.co",
+  api: "https://atomic_fec_30094Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
